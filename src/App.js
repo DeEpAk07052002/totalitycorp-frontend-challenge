5a6d55adb98a0edf6e59026b7d05f46906f89678
deepak <dg830576@gmail.com>
2023-08-31 16:43:13 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <AppHeader />
         <PageContent />
-        <AppFooter />
+        {/* <AppFooter /> */}
       </BrowserRouter>
     </div>
   );
